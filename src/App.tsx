53a8feadc410c6fcc855122029f7df65e0aaca3c
mk/1589 <mnirmalkar745@gmail.com>
2024-06-20 16:03:52 +0530

    add Shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
-import './App.css'
+
+import { Button } from '@/components/ui/button'
 
 function App() {
  
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1>wc</h1>
+      <Button>Click me</Button>
       
     </>
   )
